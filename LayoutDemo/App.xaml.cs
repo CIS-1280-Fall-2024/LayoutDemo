a72e60380a67c5c54b3275ecdba075f6026ae1bd
Robert Garner <rgarner011235@gmail.com>
2024-11-07 18:00:18 -0700

    Add project files.

---
+++ LayoutDemo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LayoutDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
